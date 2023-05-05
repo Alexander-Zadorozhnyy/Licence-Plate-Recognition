4128d80d3618864f5bd57d61141afe2b7f20aaff
Alexander-Zadorozhnyy <sasha.zadorojnii2013@yandex.ru>
2023-05-05 12:56:15 +0300

    Removed unnecessary CTCLayer

--- src/recognition/model/model_utils.py
+++ src/recognition/model/model_utils.py
@@ -1,28 +1,5 @@
 import tensorflow as tf
-import tensorflow.keras.backend as K
 from tensorflow.keras import layers
-
-
-class CTCLayer(layers.Layer):
-    def __init__(self, name=None):
-        super().__init__(name=name)
-        self.loss_fn = K.ctc_batch_cost
-
-    def call(self, y_true, y_pred):
-        # Compute the training-time loss value and add it
-        # to the layer using `self.add_loss()`.
-        batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-        input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-        label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
-
-        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-        label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-
-        loss = self.loss_fn(y_true, y_pred, input_length, label_length)
-        self.add_loss(loss)
-
-        # At test time, just return the computed predictions
-        return y_pred
 
 
 class LightDepthwiseSeparableConvResidualBlock(tf.keras.layers.Layer):
