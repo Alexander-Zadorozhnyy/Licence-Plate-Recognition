40fbbe0f533d350222f00101d9ef75247ca12c4d
Alexander-Zadorozhnyy <sasha.zadorojnii2013@yandex.ru>
2023-05-19 12:59:05 +0300

    Realized dataprovider

---
+++ src/recognition/dataset/DataProviders.py
@@ -0,0 +1,86 @@
+import copy
+import tensorflow as tf
+
+import numpy as np
+from mltu.dataProvider import DataProvider
+import logging
+
+from dataset.filters import GrayFilter
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s')
+
+
+class MyDataProvider(DataProvider, tf.keras.utils.Sequence):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def process_data(self, batch_data):
+        """ Process data batch of data """
+        if self._use_cache and batch_data[0] in self._cache:
+            data, annotation = copy.deepcopy(self._cache[batch_data[0]])
+        else:
+            data, annotation = batch_data
+            for preprocessor in self._data_preprocessors:
+                data, annotation = preprocessor(data, annotation)
+
+            if data is None or annotation is None:
+                self.logger.warning("Data or annotation is None, marking for removal on epoch end.")
+                self._on_epoch_end_remove.append(batch_data)
+                return None, None
+
+            if self._use_cache and batch_data[0] not in self._cache:
+                self._cache[batch_data[0]] = (copy.deepcopy(data), copy.deepcopy(annotation))
+
+        data_s, annotation_s = copy.deepcopy(data), copy.deepcopy(annotation)
+
+        # Then augment, transform and postprocess the batch data
+        for objects in [self._augmentors, self._transformers]:
+            for object in objects:
+                data, annotation = object(data, annotation)
+
+        for object in self._transformers:
+            data_s, annotation_s = object(data_s, annotation_s)
+
+        to_gray_scale = GrayFilter()
+        data_s, annotation_s = to_gray_scale(data_s, annotation_s)
+
+        # Convert to numpy array if not already
+        if not isinstance(data, np.ndarray):
+            data = data.numpy()
+
+        # Convert to numpy array if not already
+        # TODO: This is a hack, need to fix this
+        if not isinstance(annotation, (np.ndarray, int, float, str, np.uint8, np.float)):
+            annotation = annotation.numpy()
+
+        # Convert to numpy array if not already
+        if not isinstance(data_s, np.ndarray):
+            data_s = data_s.numpy()
+
+        # Convert to numpy array if not already
+        # TODO: This is a hack, need to fix this
+        if not isinstance(annotation_s, (np.ndarray, int, float, str, np.uint8, np.float)):
+            annotation_s = annotation_s.numpy()
+
+        return [(data_s, annotation_s), (data, annotation)]
+
+    def __getitem__(self, index: int):
+        """ Returns a batch of data by batch index"""
+        dataset_batch = self.get_batch_annotations(index)
+
+        # First read and preprocess the batch data
+        batch_data, batch_annotations = [], []
+        for index, batch in enumerate(dataset_batch):
+            (data_s, annotation_s), (data, annotation) = self.process_data(batch)
+
+            if data is None or annotation is None:
+                self.logger.warning("Data or annotation is None, skipping.")
+                continue
+
+            batch_data.append(data_s)
+            batch_annotations.append(annotation_s)
+
+            batch_data.append(data)
+            batch_annotations.append(annotation)
+
+        return np.array(batch_data), np.array(batch_annotations)
---
+++ src/recognition/dataset/filters.py
@@ -0,0 +1,82 @@
+import cv2
+from mltu import Image
+from mltu.augmentors import Augmentor, randomness_decorator
+import typing
+import numpy as np
+import logging
+
+
+class GrayFilter(Augmentor):
+    """ Adjust grayscale to image """
+
+    def __init__(
+            self,
+            log_level: int = logging.INFO,
+    ) -> None:
+        """ Randomly adjust image brightness
+
+        Args:
+            random_chance (float, optional): Chance of applying the augmentor. Where 0.0 is never and 1.0 is always. Defaults to 0.5.
+            delta (int, optional): Integer value for brightness adjustment. Defaults to 100.
+            log_level (int, optional): Log level for the augmentor. Defaults to logging.INFO.
+        """
+        super(GrayFilter, self).__init__(1, log_level)
+
+    @randomness_decorator
+    def __call__(self, image: Image, annotation: typing.Any) -> typing.Tuple[Image, typing.Any]:
+        """ Adjust grayscale to image
+
+        Args:
+            image (Image): Image to be adjusted
+            annotation (typing.Any): Annotation to be adjusted
+
+        Returns:
+            image (Image): Adjusted image
+            annotation (typing.Any): Adjusted annotation if necessary
+        """
+
+        img = np.array(image.RGB(), dtype=np.float32)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+        image.update(img)
+
+        return image, annotation
+
+
+class EdgeFilter(Augmentor):
+    """ Adjust grayscale to image """
+
+    def __init__(
+            self,
+            log_level: int = logging.INFO,
+    ) -> None:
+        """ Randomly adjust image brightness
+
+        Args:
+            random_chance (float, optional): Chance of applying the augmentor. Where 0.0 is never and 1.0 is always. Defaults to 0.5.
+            delta (int, optional): Integer value for brightness adjustment. Defaults to 100.
+            log_level (int, optional): Log level for the augmentor. Defaults to logging.INFO.
+        """
+        super(EdgeFilter, self).__init__(1, log_level)
+
+    @randomness_decorator
+    def __call__(self, image: Image, annotation: typing.Any) -> typing.Tuple[Image, typing.Any]:
+        """ Adjust grayscale to image
+
+        Args:
+            image (Image): Image to be adjusted
+            annotation (typing.Any): Annotation to be adjusted
+
+        Returns:
+            image (Image): Adjusted image
+            annotation (typing.Any): Adjusted annotation if necessary
+        """
+
+        img = np.array(image.RGB(), dtype=np.float32)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img_blur = cv2.GaussianBlur(img, (3, 3), 0)
+        img = cv2.Canny(img_blur, 25, 40)
+
+        image.update(img)
+
+        return image, annotation
